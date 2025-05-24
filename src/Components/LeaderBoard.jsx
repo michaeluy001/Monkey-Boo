23d959c8ef97d89e52dc7cd6fd177c9d0ed7f60b
michael_uy <microfarad043@gmail.com>
2025-05-24 15:33:53 +0800

    fix: list container height

--- src/Components/LeaderBoard.jsx
+++ src/Components/LeaderBoard.jsx
@@ -9,7 +9,6 @@ const LeaderBoard = () => {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
-
   useEffect(() => {
     const fetchPlayers = async () => {
       try {
@@ -30,10 +29,13 @@ const LeaderBoard = () => {
 
   return (
     <>
-      <div className="bg-green-800 h-2/3 overflow-hidden ">
+      <div className="bg-green-800  max-w-[600px] m-auto rounded-3xl">
         <div className=" h-35 w-full  justify-items-center p-4 flex flex-col text-white ">
           <div className="text-2xl mt-5 justify-items-end">
-            <ReturnButton > <IoMdClose/> </ReturnButton>
+            <ReturnButton>
+              {" "}
+              <IoMdClose />{" "}
+            </ReturnButton>
           </div>
           <div className="text-center  font-bold text-2xl">--Leaderboard--</div>
           <div className="text-sm grid grid-cols-4 gap-10 justify-items-center my-3 text-center">
@@ -43,15 +45,17 @@ const LeaderBoard = () => {
           </div>
         </div>
 
-        <div className="p-4 text-gray-900/70">
-          <ul className="w-full  mx-auto h-65 overflow-scroll bg-green-400/30 inset-shadow-sm inset-shadow-green-800">
+        <div className="h-80 p-5 ">
+          <ul className="w-full rounded-2xl  overflow-y-scroll  h-full  bg-green-400/30 inset-shadow-sm inset-shadow-green-800 ">
             {players.map((player, index) => (
               <li
                 key={index}
                 className=" content-center h-8 grid grid-cols-4 gap-10 justify-items-center border-b-2"
               >
                 <p className="">{index + 1}</p>
-                <p className="line-clamp-2 w-30 text-center">{player.playername}</p>{" "}
+                <p className="line-clamp-2 w-30 text-center">
+                  {player.playername}
+                </p>{" "}
                 <p className="">{player.playerscore}</p>
                 <p className="content-center">
                   {index === 0 && <GoTrophy className="text-amber-300" />}
