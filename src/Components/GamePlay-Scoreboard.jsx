8de03700f786fbfb3c9229df2ba8f81ef4927403
michael_uy <microfarad043@gmail.com>
2025-05-24 12:20:47 +0800

    feat(GamePlay-Scoreboard): add max-w-[600px]. add rounded. remove bg color. replace text-color to yelllw-500

--- src/Components/GamePlay-Scoreboard.jsx
+++ src/Components/GamePlay-Scoreboard.jsx
@@ -3,20 +3,20 @@ import { motion } from "motion/react";
 const Scoreboard = (props) => {
   return (
     <>
-      
       <motion.div
-        className="h-20 w-full fixed top-0 bg-amber-200 content-center items-center justify-items-center px-5 grid grid-cols-3 "
+        className="h-20 w-full fixed top-0  content-center items-center justify-items-center "
         initial={{ top: -100 }}
-        animate={{ top: 0 }}
-        transition={{ duration: 0.5 }}
+        animate={{ top: 0,  }}
+        transition={{ duration: 0.5, type:'spring' }}
       >
-        <ReturnButton className="text-3xl" />
-        <div className="text-center text-xs">
-          Current Score <p className="text-lg">{props.gameScore}</p>
-        </div>
-        <div className="text-center flex flex-col">
-         
-          <p>Level</p> <p>{props.level}</p>
+        <div className="max-w-[600px] border grid grid-cols-3 m-auto items-center text-yellow-500 justify-items-center text-lg whitespace-nowrap py-1 rounded-4xl">
+          <ReturnButton className="text-3xl" />
+          <div className="text-center text-lg ">
+            Current Score <p className="text-lg">{props.gameScore}</p>
+          </div>
+          <div className="text-center flex flex-col">
+            <p>Round</p> <p>{props.level}</p>
+          </div>
         </div>
       </motion.div>
       ;
